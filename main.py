cfed998ab70c9c244ef432aaa458d29fb658018b
Lee <charismafight@hotmail.com>
2021-09-21 00:19:19 +0800

    finish the base workflow,need to call api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests as requests
 from loguru import logger
+import time
 
 logger.add('UpdateHomeIpAddressToCloud.log')
 ip = '221.232.174.157'
@@ -8,9 +9,15 @@ if __name__ == '__main__':
     logger.debug('UpdateHomeIpAddressToCloud started')
     while True:
         result = requests.get('http://myip.ipip.net/s')
-        if result.status_code == 200 and result.text != ip:
-            logger.info('ip changed to ' + result.text)
-            ip = result.text
-            # call txcloud api
+        if result.status_code == 200:
+            if result.text.strip() != ip:
+                logger.info('ip:' + ip)
+                logger.info('result:' + result.text)
+                ip = result.text
+                # call txcloud api
+            else:
+                logger.info('ip has not changed')
         else:
-            print('fuck')
+            logger.info('can not visit http://myip.ipip.net/s')
+
+        time.sleep(20)
